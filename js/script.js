2b3700c37fb4d02b075a157fcac93be9be8febcf
drums1706 <63394376+drums1706@users.noreply.github.com>
2020-05-10 18:14:02 +0200

    Scroll down button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
-    $('a[href*=#]').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+    $('.scroll-down').click(function() {
+        $('html, body').animate({ scrollTop: $('section.ok').offset().top }, 'slow');
+        return false;
     });
 });
